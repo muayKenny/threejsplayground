f560a3efeb2ff266aaf0545febaa5ed9ae50445d
kenny <kenny@kshefs-MacBook-Pro.local>
2018-11-14 18:09:07 -0800

    more heroku config

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+//const port = 3000;
 var path = require('path');
 app.use(express.static('public'))
 
 app.get('/', (req, res) =>     res.sendFile(path.join(__dirname + '/index.html')));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(() => console.log(`three.js playground app running on !`));
