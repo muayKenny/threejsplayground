dba999fd97334b573a104f586c3a0acd82d972fb
kenny <kenny@kshefs-MacBook-Pro.local>
2018-11-14 18:29:43 -0800

    heroku configuration v

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) =>     res.sendFile(path.join(__dirname + '/index.html')));
 
-app.listen(() => console.log(`three.js playground app running on !`));
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
